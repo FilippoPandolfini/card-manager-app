d537a2fa8f9739117abe2eb03c2bcd09c272eeb1
Filippo Pandolfini <pandolfini.filippo@gmail.com>
2025-05-21 09:50:24 +0200

    Initial commit - Spring Boot project

---
+++ cardapp/src/test/java/card_manager/cardapp/CardappApplicationTests.java
@@ -0,0 +1,13 @@
+package card_manager.cardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
